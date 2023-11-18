a0e2e2d13ed7ed67e1b8d2911f205116a35a27f7
Hylander21st <110047213+Hylander21st@users.noreply.github.com>
2023-11-18 17:01:31 +0800

    Update Alden_animelist.js

--- Alden_animelist.js
+++ Alden_animelist.js
@@ -47,6 +47,30 @@ const AnimeStates = [
         averagerating: 4.4,
         studio: 'Bones',
     },
+    {
+        id: 3,
+        title: 'Jujutsu Kaisen',
+        genre: 'Action, Demons, School',
+        episodes: 48,
+        description: 'In a world where demons feed on unsuspecting humans, fragments of the legendary and feared demon Ryoumen Sukuna were lost and scattered about. ',
+        releaseYear: 2020,
+        reviews: [
+            {
+                rating: 9.0,
+                comments: 'Good Action',
+            },
+            {
+                rating: 6.2,
+                comments: 'Predictable',
+            },
+            {
+                rating: 2.0,
+                comments: 'Got Bored',
+            },
+        ],
+        averagerating: 5.7,
+        studio: 'Bones',
+    },
 ];
 
 //GETTER
@@ -63,9 +87,11 @@ function getAllSeries() {
         //creates and returns a new array with just the title and rating properties
         .map(anime => ({ title: anime.title, rating: anime.averagerating || 0.0 }));
 }
+
 //function to get an anime by its title
 function getAnimebyTitle(title) {
-    let animename = AnimeStates.filter(s => s.title === title)
+    // inlcudes so that even if not exact, will still pop up
+    let animename = AnimeStates.filter(s => s.title.includes(title))
     if (animename) {
         //s is curreent instance
         //runs through each instance to match s.title== current title
@@ -76,26 +102,39 @@ function getAnimebyTitle(title) {
 
 }
 
+//function to get the comments on an anime
 function getCommentsByTitle(title) {
-    // Find the anime with the specified title
+    // find the anime with the specified title
     const anime = AnimeStates.find(anime => anime.title === title);
 
-    // Check if the anime is found
+    // check if anime is found
     if (!anime) {
-        return []; // Anime not found, return an empty array
+        // if not found, return error
+        return "There is no anime by that name";
+    } else {    // get comments from the reviews
+        const comments = anime.reviews.map(review => review.comments);
+
+        // filter out empty comments
+        return comments.filter(comment => comment !== '');
     }
 
-    // Extract comments from the reviews
-    const comments = anime.reviews.map(review => review.comments);
 
-    // Filter out empty comments
-    return comments.filter(comment => comment !== '');
 }
 
-
+//function to get a random anime
+function getRandomAnime() {
+    // to just get the range of inedexes possible etc 1 to 3
+    const animeCount = AnimeStates.length;
+    // random number generator
+    //.floor rounds nearest integer as math.random generates a float number
+    // multiply by anime count meaning it will be 1 - anime count
+    const randomIndex = Math.floor(Math.random() * animeCount);
+    const randomAnime = AnimeStates[randomIndex];
+    return randomAnime;
+}
 
 //POSTER
-
+//function to add an anime into the array
 function addAnime(id, title, genre, episodes, description, releaseYear, studio) {
     AnimeStates.push({
         id: id,
@@ -110,30 +149,39 @@ function addAnime(id, title, genre, episodes, description, releaseYear, studio) 
     });
 }
 
-function addReview(id, rating, review) {
+//function to add a review which will add to the rating and comments
+function addReview(id, rating, comment) {
     let anime = AnimeStates.find(a => a.id == id)
 
     if (anime) {
-        // Add the review to the reviews array
-        anime.reviews.push({
-            rating: rating,
-            comments: review
-        });
-        // let averageRating = 0;
-        const sum = anime.reviews.length
-        // calc total rating from all reviews
-        const total = anime.reviews.reduce((a, b) => a + b.rating, 0)
-        anime.averagerating = total / sum
-        return "Your rating of " + rating + " and comment:" + review + " has been added"
+        // rating has to be 1 to 10
+        if (rating >= 1 && rating <= 10) {
+            // add the review to the reviews array
+            anime.reviews.push({
+                rating: rating,
+                comments: comment
+            });
+            const sum = anime.reviews.length;
+            // calc total rating from all reviews
+            // reduce adds up all the ratings 
+            // a is initially 0, b is the rating they add to sum it up
+            const total = anime.reviews.reduce((a, b) => a + b.rating, 0);
+            anime.averagerating = total / sum;
+            return "Your rating of " + rating + " and comment: " + comment + " has been added";
+        } else {
+            return "Please enter a rating between 1 and 10";
+        }
+    } else {
+        return "Anime not found";
     }
-
-
 }
 
+//function to edit an anime's details
 function editAnimebyID(id, title, genre, episodes, description, releaseYear, studio) {
+    //find the anime you want to edit through id
     let anime = AnimeStates.find(a => a.id == id)
     if (anime) {
-
+        //change the details with the new inputted ones
         anime.title = title,
             anime.genre = genre,
             anime.episodes = episodes,
@@ -144,9 +192,11 @@ function editAnimebyID(id, title, genre, episodes, description, releaseYear, stu
 
     }
 }
+
 //DELETE
+//function to delete an anime using id
 function deleteAnimebyID(id) {
-    AnimeStates.splice(id, 1)
+    AnimeStates.splice(id - 1, 1)
     return AnimeStates
 }
 
@@ -158,4 +208,5 @@ module.exports = {
     deleteAnimebyID,
     getCommentsByTitle,
     editAnimebyID,
+    getRandomAnime,
 }
